6cde4937b7e6f7de26e9074887a5b695e0b39f8a
Victor Socaciu <socaciu.victor@gmail.com>
2020-11-23 21:49:39 +0200

    Initial Commit

---
+++ ISODial/ISODialApp.swift
@@ -0,0 +1,17 @@
+//
+//  ISODialApp.swift
+//  ISODial
+//
+//  Created by Victor Socaciu on 23.11.2020.
+//
+
+import SwiftUI
+
+@main
+struct ISODialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
